8ca8a4246cddc02ce1a1111fa2e0eb8528c20167
Voingod <43098874+Voingod@users.noreply.github.com>
2019-01-21 11:05:37 +0200

    Добавил проект libExcel

---
+++ libExcel/libExcel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace libExcel
+{
+    class libExcel
+    {
+        static void Main(string[] args)
+        {
+            Test();
+            Read read = new Read();
+            read.ReadFile();
+        }
+
+        private static void Test()
+        {
+            Console.WriteLine("Test");
+           // Console.ReadLine();
+        }
+    }
+
+    public class Read
+    {
+        public void ReadFile ()
+        {
+            Console.WriteLine("read");
+           
+            Write();
+            Console.ReadLine();
+        }
+
+        private void Write()
+        {
+
+            Console.WriteLine("write");
+        }
+    }
+}
