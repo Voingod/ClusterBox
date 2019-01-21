7d20adc31a59c7bd46db7d87730ac32f9873f13b
Voingod <43098874+Voingod@users.noreply.github.com>
2019-01-21 10:20:18 +0200

    Изменил Form1 на StartWindow

--- ReadExcel/Program.cs
+++ ReadExcel/Program.cs
@@ -16,7 +16,7 @@ namespace ReadExcel
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new StartWindow());
         }
     }
 }
