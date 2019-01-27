d769243890d91561215002cc46833dcfa4f3c0a9
Voingod-Home <voingod@outlook.com>
2019-01-27 19:11:46 +0200

    Добавил метод Select

--- libExcel/libExcel.cs
+++ libExcel/libExcel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.OleDb;
 
 namespace libExcel
 {
@@ -6,32 +8,38 @@ namespace libExcel
     {
         static void Main(string[] args)
         {
-            Test();
-            Read read = new Read();
-            read.ReadFile();
-        }
-
-        private static void Test()
-        {
-            Console.WriteLine("Test");
-           // Console.ReadLine();
-        }
-    }
-
-    public class Read
-    {
-        public void ReadFile ()
-        {
-            Console.WriteLine("read");
-           
-            Write();
+            
+            string path= @"D:\ForExcelTest.xlsx";
+            string sheet= "Random";
+            string value="One";
+            DataTable dt = Select(path,sheet,value);
+            double dataExcelGlobalFormat = 0;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                dataExcelGlobalFormat = Convert.ToDouble(dt.Rows[i][0].ToString());
+                Console.WriteLine(dataExcelGlobalFormat);
+            }
             Console.ReadLine();
         }
 
-        private void Write()
-        {
-
-            Console.WriteLine("write");
+        /// <summary>
+        /// Метод для считывания данных с Excel файлов
+        /// </summary>
+        /// <param name="path">Пусть к файлу</param>
+        /// <param name="sheet">Лист для чтения</param>
+        /// <param name="value">Название столбцов для чтения</param>
+        /// <returns></returns>
+        static DataTable Select (string path, string sheet, string value)
+        {            
+            string stringcoon = " Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";" + "Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=1;'";
+            OleDbConnection conn = new OleDbConnection(stringcoon);
+            OleDbDataAdapter da = new OleDbDataAdapter("Select "+value+" from[" + sheet + "$]", conn);
+            DataTable dt = new DataTable("Read");
+            da.Fill(dt);
+            return dt;
         }
+
+
     }
+
 }
