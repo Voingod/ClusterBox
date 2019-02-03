41235d05d42a3cf713ea870fdc2bb5925da67aed
Voingod-Home <voingod@outlook.com>
2019-02-03 15:02:24 +0200

    Настраивание обработки исключений для метода считывания Перевел на более раннюю версию .Net 4.5 для русских подсказок в студии

--- libExcel/libExcel.cs
+++ libExcel/libExcel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.OleDb;
+using System.Windows.Forms;
 
 namespace libExcel
 {
@@ -9,7 +10,7 @@ namespace libExcel
         static void Main(string[] args)
         {
             
-            string path= @"D:\ForExcelTest.xlsx";
+            string path= @"D:\F2orExcelTest.xlsx";
             string sheet= "Random";
             string value="One";
             DataTable dt = Select(path,sheet,value);
@@ -30,12 +31,33 @@ namespace libExcel
         /// <param name="value">Название столбцов для чтения</param>
         /// <returns></returns>
         static DataTable Select (string path, string sheet, string value)
-        {            
-            string stringcoon = " Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";" + "Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=1;'";
-            OleDbConnection conn = new OleDbConnection(stringcoon);
-            OleDbDataAdapter da = new OleDbDataAdapter("Select "+value+" from[" + sheet + "$]", conn);
+        {
             DataTable dt = new DataTable("Read");
-            da.Fill(dt);
+            try
+            {
+                string stringcoon = " Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";" + "Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=1;'";
+                OleDbConnection conn = new OleDbConnection(stringcoon);
+                OleDbDataAdapter da = new OleDbDataAdapter("Select " + value + " from[" + sheet + "$]", conn);
+                
+                da.Fill(dt);
+                
+            }
+            catch (OleDbException ex)
+            {
+                int i = 1;
+                int j = 2;
+
+                 var abc = ex.ErrorCode == -2147467259 ? i=3 : j=4;
+
+    //            Console.WriteLine("Файл {0} не знайдено. Перевірте наявність файлу {0}.\n" +
+    //"Якщо файл існує, перевірте коректність введеного шляху", path);
+
+                MessageBox.Show("Файл "+ path.Remove(path.LastIndexOf('\\'), path.Length - path.LastIndexOf('\\'))
+                +" не знайдено. Перевірте наявність файлу " +path+".\n" +
+                    "Якщо файл існує, перевірте коректність введеного шляху");
+                
+
+            }
             return dt;
         }
 
