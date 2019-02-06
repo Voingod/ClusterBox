1f5950ee7315ed2f992a39ed6af29c880d418327
Voingod <43098874+Voingod@users.noreply.github.com>
2019-02-06 23:07:19 +0200

    Обошел try-catch, обработал ситуации, когда нет листа или файла

--- libExcel/libExcel.cs
+++ libExcel/libExcel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
+using System.IO;
 using System.Windows.Forms;
 
 namespace libExcel
@@ -10,14 +12,34 @@ namespace libExcel
         static void Main(string[] args)
         {
             
-            string path= @"D:\F2orExcelTest.xlsx";
+            string path= @"D:\ForExcelTest.xlsx";
             string sheet= "Random";
-            string value="One";
+            string value="One,Two";
+
             DataTable dt = Select(path,sheet,value);
-            double dataExcelGlobalFormat = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Для відлагодження
+            string dataExcelGlobalFormat = "";
             for (int i = 0; i < dt.Rows.Count; i++)
             {
-                dataExcelGlobalFormat = Convert.ToDouble(dt.Rows[i][0].ToString());
+                dataExcelGlobalFormat = Convert.ToString(dt.Rows[i][0]);
                 Console.WriteLine(dataExcelGlobalFormat);
             }
             Console.ReadLine();
@@ -35,28 +57,91 @@ namespace libExcel
             DataTable dt = new DataTable("Read");
             try
             {
-                string stringcoon = " Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";" + "Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=1;'";
-                OleDbConnection conn = new OleDbConnection(stringcoon);
-                OleDbDataAdapter da = new OleDbDataAdapter("Select " + value + " from[" + sheet + "$]", conn);
-                
-                da.Fill(dt);
-                
+                if (File.Exists(path))
+                {
+                    string stringcoon = " Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";" + "Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=1;'";
+                    OleDbConnection conn = new OleDbConnection(stringcoon);
+                    OleDbCommand cmd = new OleDbCommand();
+                    cmd.Connection = conn;
+                    conn.Open();
+                    DataTable schemaTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+
+
+                    // Показать список листов в файле
+                    List<string> ExcelSheets = new List<string>();
+                    List<string> ColumnInSheets = new List<string>();
+
+
+                    for (int i = 0; i < schemaTable.Rows.Count; i++)
+                    {
+                        string str = Convert.ToString(schemaTable.Rows[i].ItemArray[2]);
+                        string[] charsToRemove = new string[] { "$" };
+                        foreach (string c in charsToRemove)
+                        {
+                            str = str.Replace(c, string.Empty);
+                        }
+                        ExcelSheets.Add(str);
+                    }
+
+                    if (ExcelSheets.Contains(sheet))
+                    {
+                        int indexsheet = ExcelSheets.IndexOf(sheet);
+                        string sheet1 = (string)schemaTable.Rows[indexsheet].ItemArray[2];
+                        string select = String.Format("SELECT * FROM [{0}]", sheet1);
+
+                        OleDbCommand oleDB = new OleDbCommand(select, conn);
+                        OleDbDataReader reader = oleDB.ExecuteReader();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            ColumnInSheets.Add(reader.GetName(i)); // Имя столбца
+                        }
+
+                        if (ColumnInSheets.Contains(value))
+                        {
+                            OleDbDataAdapter da = new OleDbDataAdapter(" Select " + value + " from[" + sheet + "$]", conn);
+                            da.Fill(dt);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Таблиця (лист) " + sheet + " не містить стовпчика (колонки) " + value);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Файл " + path.Remove(0, path.IndexOf('\\') + 1) +
+                            " не містить таблиці (листа) " + sheet);
+                    }
+                    conn.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Файл " + path.Remove(0, path.IndexOf('\\') + 1) + " не знайдено." +
+                    "Перевірте наявність файлу " + path.Remove(0, path.IndexOf('\\') + 1) + ".\n" +
+                    "Якщо файл існує, перевірте коректність введеного шляху: " +
+                    path.Remove(path.LastIndexOf('\\'), path.Length - path.LastIndexOf('\\')) + "");
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                if (ex.HResult == -2146233079)
+                {
+                    MessageBox.Show("Необхідно встановити додаток AccessDatabaseEngine. \n" +
+                        "(Для роботи з Excel файлами як файлами бази даних)");
+                }
+                else
+                {
+                    MessageBox.Show("Необроблена помилка!!!\n" + ex.Message);
+                }
+
             }
             catch (OleDbException ex)
             {
-                int i = 1;
-                int j = 2;
+                MessageBox.Show("Необроблена помилка!!!\n" + ex.Message);
 
-                 var abc = ex.ErrorCode == -2147467259 ? i=3 : j=4;
-
-    //            Console.WriteLine("Файл {0} не знайдено. Перевірте наявність файлу {0}.\n" +
-    //"Якщо файл існує, перевірте коректність введеного шляху", path);
-
-                MessageBox.Show("Файл "+ path.Remove(path.LastIndexOf('\\'), path.Length - path.LastIndexOf('\\'))
-                +" не знайдено. Перевірте наявність файлу " +path+".\n" +
-                    "Якщо файл існує, перевірте коректність введеного шляху");
-                
-
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Необроблена помилка!!!\n"+ex.Message);
             }
             return dt;
         }
